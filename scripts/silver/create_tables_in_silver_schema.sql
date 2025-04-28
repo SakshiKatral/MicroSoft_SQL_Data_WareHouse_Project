b3bfb82b32ceada3414de1458aa96d2d5f72ae42
Sakshi Santosh Katral <67738725+SakshiKatral@users.noreply.github.com>
2025-04-28 14:27:24 +0530

    Create create_tables_in_silver_schema.sql

---
+++ scripts/silver/create_tables_in_silver_schema.sql
@@ -0,0 +1,107 @@
+--Drop 'silver.crm_customer_info' table if present 
+IF OBJECT_ID('silver.crm_customer_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_customer_info;
+GO 
+
+--Create Table silver.crm_customer_info
+CREATE TABLE silver.crm_customer_info (
+    cst_id             INT,
+    cst_key            NVARCHAR(50),
+    cst_firstname      NVARCHAR(50),
+    cst_lastname       NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr           NVARCHAR(50),
+    cst_create_date    DATE,
+    dwh_create_date    DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+--Drop 'silver.crm_product_info' table if present 
+IF OBJECT_ID('silver.crm_product_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_product_info;
+GO 
+
+--Create Table silver.crm_product_info
+CREATE TABLE silver.crm_product_info (
+ prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+--Drop 'silver.crm_sales_details' table if present 
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO 
+
+--Create Table silver.crm_sales_details
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num		NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+GO
+
+--Drop 'silver.erp_customer_az12' table if present 
+IF OBJECT_ID('silver.erp_customer_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_customer_az12;
+GO 
+
+--Create Table silver.erp_customer_az12
+CREATE TABLE silver.erp_customer_az12 (
+	cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
+
+--Drop 'silver.erp_loc_a101' table if present 
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO 
+
+--Create Table silver.erp_loc_a101
+CREATE TABLE silver.erp_loc_a101 (
+	 cid             NVARCHAR(50),
+    cntry           NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+GO
+
+
+--Drop 'silver.erp_px_cat_g1v2' table if present 
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO 
+
+--Create Table silver.erp_px_cat_g1v2
+CREATE TABLE silver.erp_px_cat_g1v2 (
+id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+GO
